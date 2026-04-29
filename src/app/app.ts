3f0306cedfd92767ec081240b2b14e1da7b6e78b
shreyasshettyb <shreyasb52@gmail.com>
2026-04-29 22:25:51 +0530

    Initial Working Build

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgbNav } from '@ng-bootstrap/ng-bootstrap/nav';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, NgbNav, NgbModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('walk_calc');
+  age: number = 0;
+  weight: number = 0;
+  height: number = 0;
+  gender: string = "";
+  calDist: number = 0;
+  actDist: number = 0;
+  deviation: number = 0;
+
+  calculate() {
+    if (this.gender === "male") {
+      this.calDist = 561.022 - (2.507 * this.age) + (1.505 * this.weight) - (0.055 * this.height)
+    }
+    else {
+      this.calDist = 30.325 * (0.0809 * this.age) - (2.074 * this.weight) + (4.235 * this.height)
+    }
+
+    if (this.actDist != 0) {
+      this.deviation = (this.actDist / this.calDist) * 100;
+    }
+
+    this.calDist = +this.calDist.toFixed(3)
+    this.deviation = +this.deviation.toFixed(3)
+  }
 }
+
